939c5b52aeafdd2ae46591ee1b6bcc928aea621e
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-07-07 20:34:04 -0700

    update app.js, getting 403 again

--- src/app.js
+++ src/app.js
@@ -14,11 +14,30 @@ function Task(){
     const _getTasks = () => {
          setTasks(mockData)
         // fetch from api
-        fetch( proxyurl + API)
+        fetch( `${proxyurl}${API}` )
         .then( data => data.json() )
         .then( allTasks => setTasks(allTasks) )
         .catch( console.error);
       };
+
+     const _toggleStatus = (e) => {
+        e.preventDefault();
+        let id = e.target.id;
+
+        fetch(`${proxyurl}${API}/${id}/state`, {
+            mode: 'cors',
+            method: 'PATCH'
+        })
+        .then( data => data.json() )
+        .then( singleTask => {
+            setTasks( task.map( (entry) => {
+//                console.log(entry.id);
+                return entry.id === id ? singleTask : entry;
+                }
+            ));
+        })
+        .catch( console.error );
+     };
 
     useEffect(_getTasks, []);
 
@@ -31,15 +50,32 @@ function Task(){
             <li key={oneTask.id}>
               <details>
                 <summary>
-                  <p>{oneTask.title}</p>
-                  <p>{oneTask.description}</p>
+                  <p>{oneTask.title} | Assignee: {oneTask.assignee}</p>
+                  <p>Status: <span id={oneTask.id} onClick={_toggleStatus}>{oneTask.status.toString()}</span>
+                  </p>
 
                 </summary>
+                <Description description={oneTask.description} />
               </details>
             </li>
           )}
          </ul>
    </>
+    )
+}
+
+function Description(props){
+    let description = props.description || [];
+    return (
+
+        <section>
+            <>
+                <p>Description: <span>{description}</span></p>
+            </>
+        </section>
+
+
+
     )
 }
 
